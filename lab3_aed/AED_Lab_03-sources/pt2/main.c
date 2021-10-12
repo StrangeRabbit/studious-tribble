4f74dab72f0f88c34426044de84dd79c0629f70a
Gonçalo Frazão <gonfrazao@gmail.com>
2021-10-12 01:00:53 +0100

    3.0

--- lab3_aed/AED_Lab_03-sources/pt2/main.c
+++ lab3_aed/AED_Lab_03-sources/pt2/main.c
@@ -64,7 +64,7 @@ int main(int argc, char *argv[])
     Usage(argv[0]);
 
   nomeFicheiroIn = argv[1];
-  nomeFicheiroOut = /* -- INSERT CODE to ALLOCATE MEMORY -- */
+  nomeFicheiroOut = (char*) malloc(strlen(argv[1]) + 1 + strlen(extOut));
     if(nomeFicheiroOut == NULL)
       erroMemoria("Memory allocation for nomeFicheiroOut in main" );
 
@@ -105,8 +105,9 @@ int main(int argc, char *argv[])
   libertaLista(lp, libertaItem);
 
   /* -- CLOSE ALL OPEN FILES -- */
-
+  fclose(fpIn);
+  fclose(fpOut);
   /* -- FREE ANY OTHER MEMORY YOU HAVE ALLOCATED -- */
-
+  free(nomeFicheiroOut);
   exit(0);
 }
